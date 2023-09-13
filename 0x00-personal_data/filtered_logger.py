eed7d1e22d2f9fdba84ac8e5062e0d3643b71eeb
Njiraini23 <njiraini88@gmail.com>
2023-09-13 11:55:50 +0000

    README.md

---
+++ 0x00-personal_data/filtered_logger.py
@@ -0,0 +1,6 @@
+#!/usr/bin/env python3
+"""A function called filter_datum that returns
+long message obfscated"""
+def filter_dam(fields: list(str), redaction: str, message: str, separator):
+    """A function that returns the log mesage obfuscated"""
+
