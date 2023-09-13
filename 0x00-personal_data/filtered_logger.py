2de007793f79a8d662e7dd1e7cd065c351a8a948
Njiraini23 <njiraini88@gmail.com>
2023-09-13 18:09:52 +0000

    filtered_logger.py

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,6 +1,12 @@
 #!/usr/bin/env python3
 """A function called filter_datum that returns
 long message obfscated"""
-def filter_dam(fields: list(str), redaction: str, message: str, separator):
-    """A function that returns the log mesage obfuscated"""
 
+
+def filter_dam(fields: List[str], redaction: str, message: str,
+               separator: str) -> str:
+    """A function that returns the log mesage obfuscated"""
+    for field in fields:
+        message = re.sub(f'{field}=(.*?){separator}',
+                         f'{field}={redaction}{separator}', message)
+        return message
