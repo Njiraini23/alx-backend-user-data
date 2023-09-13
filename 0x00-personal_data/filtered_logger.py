c6f49294e70bcc4911230fd1575f938bf1104ec2
Njiraini23 <njiraini88@gmail.com>
2023-09-13 20:37:35 +0000

    changes

--- 0x00-personal_data/filtered_logger.py
+++ 0x00-personal_data/filtered_logger.py
@@ -1,15 +1,15 @@
 #!/usr/bin/env python3
 """A function called filter_datum that returns
 long message obfscated"""
-import logging
 from typing import List
 import re
+import logging
 
 
 def filter_datum(fields: List[str], redaction: str, message: str,
                  separator: str) -> str:
-    """A function that returns the log mesage obfuscated"""
+    """returns the log message obfuscated"""
     for field in fields:
         message = re.sub(f'{field}=(.*?){separator}',
                          f'{field}={redaction}{separator}', message)
-        return message
+    return message
