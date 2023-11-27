dcc06ed31719a1f2a1bb49e9ef5c5b884e345a3a
njiraini23 <njiraini88@gmail.com>
2023-11-27 09:53:48 +0000

    fixing the import files

--- 0x01-Basic_authentication/api/v1/app.py
+++ 0x01-Basic_authentication/api/v1/app.py
@@ -16,7 +16,7 @@ auth = None
 AUTH_TYPE = getenv('AUTH_TYPE')
 
 if AUTH_TYPE == "auth":
-    from api.V1.auth.auth import Auth
+    from api.v1.auth.auth import Auth
     auth = Auth()
 elif AUTH_TYPE == "basic_auth":
     from api.vi.auth.basic_auth import BasicAuth
