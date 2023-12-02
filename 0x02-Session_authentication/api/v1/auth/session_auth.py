f21ca3cfea17a00f41a0dbf8f7711e4e767477c8
njiraini23 <njiraini88@gmail.com>
2023-12-02 19:15:27 +0000

    session_auth.py

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -1,2 +1,56 @@
 #!/usr/bin/env python3
 """ The class SessionAuth"""
+from api.v1.auth.auth import Auth
+from models.user import User
+from typing import List, TypeVar
+import uuid
+
+
+class SessionAuth(Auth):
+    """Implement session Authorization protocol methods"""
+    user_id_by_session_id {}
+
+    def create_session(self, user_id: str = None) -> str:
+        """
+        Create a Session ID for a user_id
+        :param user_ud: User ID (string) to associate with the session
+        :return: Session ID (string)
+        """
+        if user_id is None or not isinstance(user_id, str):
+            return None
+
+        session_id = str(uuid.uuid4())
+        self.user_id_by_session_id[session_id] = user_id
+        return session_id
+
+    def user_id_for_session_id(self, session_id: str= None) -> str:
+        """
+        returns the user ID for retrieving userID
+        returns the user ID that is associated with the ID
+        """
+        if session_id is None or not isinstance(session_id, str):
+            return None
+        return self.user_id_by_session_id.get(session_id)
+
+    def current_user(self, request=None):
+        """
+        retrieiving the user instance based on a cookie value
+        """
+        session_cookie = self.session_cookie(request)
+        user_id = self.user_id_for_session_id(session_cookie)
+
+        user = User.get(user_id)
+        return user
+
+    def destry_session(self, request=None) -> bool:
+        """Deleting the user session"""
+        if request is None:
+            return False
+        session_cookie = self.session_cookie(request)
+        if session_cookie is None:
+            return False
+        user_id = self.user_id_for_session_id(session_cookie)
+        if user_id is None:
+            return False
+        del self.user_id_by_session_id[session_cookie]
+        return True
