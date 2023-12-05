75ac8d99a0e855f2c3bf98f3324a113d7b0dfa2d
Njiraini23 <njiraini88@gmail.com>
2023-12-05 13:02:00 +0000

    corrections to the class

--- 0x03-user_authentication_service/auth.py
+++ 0x03-user_authentication_service/auth.py
@@ -37,7 +37,7 @@ class Auth:
     def __init__(self):
         self._db = DB()
 
-def register_user(email: str, password: str) -> User:
+def register_user(self, email: str, password: str) -> User:
     """The Auth function that takes email and password
     Args:
         email (str): the user's email
