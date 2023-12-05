b4f0e01d9ee606928ac32cf102433e1e30cfadc3
Njiraini23 <njiraini88@gmail.com>
2023-12-05 14:29:02 +0000

    app.py

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -18,3 +18,13 @@ AUTH = Auth()
 
 
 @app.route("/", methods=["GET"], strict_slashes=False)
+def index() -> str:
+    """
+    Returns the json response
+    {"message": "Bienvenue"}
+    """
+    return jsonify({"message": "Bienvenue"})
+
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port="5000")
